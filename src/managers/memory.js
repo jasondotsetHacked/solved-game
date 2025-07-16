1d84bf54ec2c9972965ec6a612bb8d8c892a45e3
setHacked <sethacked@gmail.com>
2025-07-16 09:19:57 -0400

    Refactor initMemory to ensure Memory.creeps is initialized

--- src/managers/memory.js
+++ src/managers/memory.js
@@ -1,10 +1,17 @@
 module.exports = {
     initMemory() {
+        Memory = {};
+        if (!Memory.creeps) {
+            Memory.creeps = {};
+        }
         if (!Memory.rooms) {
             Memory.rooms = {};
         }
         if (!Memory.scouting) {
             Memory.scouting = {};
+        }
+        if (!Memory.scouting.queue) {
+            Memory.scouting.queue = [];
         }
     }
 };
