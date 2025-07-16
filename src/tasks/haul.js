a9dc274975d40a4ef80b2c8713121013b50df0a0
setHacked <sethacked@gmail.com>
2025-07-16 19:37:14 -0400

    Add task distribution and repair flow

--- src/tasks/haul.js
+++ src/tasks/haul.js
@@ -1,19 +1,22 @@
+const taskDistribution = require('managers_taskDistribution');
+
 module.exports = {
   run(creep) {
     // If creep has energy, transfer to spawn/extensions first
     if (creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0) {
       const target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
         filter: structure =>
-          (structure.structureType === STRUCTURE_SPAWN || structure.structureType === STRUCTURE_EXTENSION) &&
+          (structure.structureType === STRUCTURE_SPAWN ||
+            structure.structureType === STRUCTURE_EXTENSION) &&
           structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
       });
       if (target) {
         if (creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-          creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+          creep.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
         }
       } else {
-        // No structures need energy, switch to building
-        creep.memory.task = 'build';
+        // Nothing needs energy - choose next productive task
+        creep.memory.task = taskDistribution.chooseTask(creep.room);
       }
     } else {
       // No energy to haul, go harvest
