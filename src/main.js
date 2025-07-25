9edab3d4b6395b167577f9807ec6e62a8de108da
setHacked <sethacked@gmail.com>
2025-07-24 20:36:43 -0400

    Refactor main loop and enhance memory management with bunker configurations

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 const colony = require('managers_colony');
 const memoryManager = require('managers_memory');
 const mapVisuals = require('managers_mapVisuals');
+const roomVisuals = require('managers_roomVisuals');
+const config = require('config');
 
 memoryManager.initMemory();
 
 module.exports.loop = function () {
   memoryManager.cleanCreepsMemory();
   colony.run();
-  mapVisuals.run();
-};
+  if (Game.cpu.bucket === 10000) {
+    Game.cpu.generatePixel();
+  }
+  if (config.DEBUG === true) {
+    roomVisuals.run();
+    mapVisuals.run();
+    if (Game.time % 100 === 0) {
+      console.log(`CPU: ${Game.cpu.getUsed().toFixed(2)} / ${Game.cpu.limit} | Bucket: ${Game.cpu.bucket}`);
+    }
+  }
+}
