ffa624d695787036a1d1497744b898cb4dae05d1
setHacked <sethacked@gmail.com>
2025-07-25 10:07:27 -0400

    Remove console logs for invalid energy sources and gather errors in gatherEnergy function

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const colony = require('managers_colony');
 const memoryManager = require('managers_memory');
 const mapVisuals = require('managers_mapVisuals');
-const roomVisuals = require('managers_roomVisuals');
+// const roomVisuals = require('managers_roomVisuals');
 const config = require('config');
 
 memoryManager.initMemory();
@@ -10,10 +10,10 @@ module.exports.loop = function () {
   memoryManager.cleanCreepsMemory();
   colony.run();
   if (Game.cpu.bucket === 10000) {
-    Game.cpu.generatePixel();
+    // Game.cpu.generatePixel();
   }
   if (config.DEBUG === true) {
-    roomVisuals.run();
+    // roomVisuals.run();
     mapVisuals.run();
     if (Game.time % 100 === 0) {
       console.log(`CPU: ${Game.cpu.getUsed().toFixed(2)} / ${Game.cpu.limit} | Bucket: ${Game.cpu.bucket}`);
